3fb50200e7f59f85f1b891605d81496d54a7a860
ibahh <bahh.ibragimoff17@gmail.com>
2022-03-07 12:16:21 +0500

    bbb

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,49 @@
 import $ from 'jquery';
 import 'slick-carousel';
+
 $(function(){
 	$('.rev .body').slick({
-		dots: false,
+		dots: true,
 		autoplay: false,
 		autoplaySpeed: 15000,
-		arrows: true,
+		arrows: false,
 		fade: true,
 		infinite: true,
 		pauseOnHover: false,
 		rows: 0,
 	  });
+
+		let header = $('.top-part.head');
+		 
+		$(window).scroll(function() {
+		  if($(this).scrollTop() > 1) {
+		   header.addClass('header_fixed');
+		   
+		  } else {
+		   header.removeClass('header_fixed');
+		  }
+		});
+		$('a[href^="#"').on('click', function() {
+
+			let href = $(this).attr('href');
+		
+			$('html, body').animate({
+				scrollTop: $(href).offset().top
+			});
+			return false;
+		});
+		// if($(this).scrollTop() > 1) {
+		// 	header.css({
+		// 	  'padding': '5px 0',
+		// 	  'background': '#f6ffdb',
+		// 	  'transition': '.3s'
+		// 	});
+		// } else {
+		// 	header.css({
+		// 	  'padding': '15px 0',
+		// 	  'background': '#ffffff',
+		// 	  'transition': '.3s'
+		// 	});
+		// }
+
 })
